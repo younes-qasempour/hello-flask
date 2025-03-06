e05f798b6be572fd618dfcc08e08ef21cafea149
younes_qasempour <qasempouryounes@gmail.com>
2025-03-06 22:37:25 +0330

    Finale

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route("/bye")
+def day_bye():
+    return "Bye"
 
 
 if __name__ == '__main__':
