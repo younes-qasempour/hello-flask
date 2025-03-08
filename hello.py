1c0ab121f525864939535b84a3238d5f36306e32
younes_qasempour <qasempouryounes@gmail.com>
2025-03-08 19:07:21 +0330

    Next session

--- hello.py
+++ hello.py
@@ -1,5 +1,21 @@
 from flask import Flask
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<i>" + function() + "</i>"
+    return wrapper
+
+def make_underline(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +25,12 @@ def hello_world():
             "<img src='https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif?cid=790b7611owv4b1jws6a9yrrf97r02tyt"
             "7biyio3nh05cmucz&ep=v1_gifs_search&rid=giphy.gif&ct=g' width=200>")
 
+
+
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underline
 def bye():
     return "<p>Bye, World!</p>"
 
