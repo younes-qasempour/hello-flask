d0bfba5bd213042957ba58eb1e63aee6ded0a875
younes_qasempour <qasempouryounes@gmail.com>
2025-03-08 17:09:39 +0330

    Next session

--- hello.py
+++ hello.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def bye():
+    return "<p>Bye, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
