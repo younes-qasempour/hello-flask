5c4c59f632010c8cb9e20440df0c7c85ccbe18cc
younes_qasempour <qasempouryounes@gmail.com>
2025-03-08 17:43:59 +0330

    Next session

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "<h1 style='text-align: center'>Hello, World!</h1>"
 
 @app.route("/bye")
 def bye():
